1dfa2c679e452c81e615b529c9645741581af3ca
EddieMacD <eddie.macdonagh@gmail.com>
2021-03-16 16:04:48 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JustForGraphs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string nameA = "Bury St Edmonds";
+            const string nameB = "Stowmarket";
+            const string nameC = "Ipswitch";
+            const string nameD = "Framlingham";
+            const string nameE = "Woodbridge";
+            const string nameF = "Wickham Market";
+
+            UndirectedMatrixGraph graph = new UndirectedMatrixGraph(nameA, 0);
+            List<int> B = new List<int> { 25, 0 };
+            List<int> C = new List<int> { 45, 21, 0 };
+            List<int> D = new List<int> { 57, 0, 31, 0 };
+            List<int> E = new List<int> { 56, 0, 15, 0, 0 };
+            List<int> F = new List<int> { 0, 0, 0, 10, 9, 0 };
+
+            graph.AddNode(nameB, B);
+            graph.AddNode(nameC, C);
+            graph.AddNode(nameD, D);
+            graph.AddNode(nameE, E);
+            graph.AddNode(nameF, F);
+
+            graph.PrintMatrix();
+            Console.WriteLine("");
+
+            Console.WriteLine(graph.NeighbourCheck(nameA, nameB));
+            Console.WriteLine(graph.NeighbourDistance(nameA, nameB));
+            Console.WriteLine(graph.NeighbourCheck(nameA, nameF));
+            Console.WriteLine(graph.NeighbourDistance(nameA, nameF));
+            Console.WriteLine("");
+
+            graph.RemoveNode(nameA);
+            graph.RemoveNode(nameD);
+
+            graph.PrintMatrix();
+            Console.WriteLine();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ UndirectedMatrixGraph.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JustForGraphs
+{
+    class UndirectedMatrixGraph
+    {
+        private List<string> Nodes;
+        private List<List<int>> Matrix;
+        private int Size;
+
+        public UndirectedMatrixGraph(string node1, int selfConnection)
+        {
+            Size = 1;
+            Nodes = new List<string> { node1 };
+            Matrix = new List<List<int>> { new List<int> { selfConnection } };
+        }
+
+        public void AddNode(string nodeName, List<int> connections)
+        {
+            if (connections.Count == Size + 1)
+            {
+                for (int i = 0; i < Size; i++)
+                {
+                    Matrix[i].Add(connections[i]);
+                }
+
+                Nodes.Add(nodeName);
+                Size++;
+                Matrix.Add(connections);
+            }
+        }
+
+        public void RemoveNode(string nodeName)
+        {
+            int index = NodeLookup(nodeName);
+
+            if (index > -1)
+            {
+                Nodes.RemoveAt(index);
+                Matrix.RemoveAt(index);
+                Size--;
+
+                for (int i = 0; i < Size; i++)
+                {
+                    Matrix[i].RemoveAt(index);
+                }
+            }
+        }
+
+        public void PrintMatrix()
+        {
+            for (int i = 0; i < Size; i++)
+            {
+                Console.Write(EqualWrite(Nodes[i] + ":", 20));
+
+                for (int j = 0; j < Size; j++)
+                {
+                    Console.Write(EqualWrite(Matrix[i][j].ToString(), 6));
+                }
+
+                Console.WriteLine("");
+            }
+        }
+
+        public bool NeighbourCheck(string nodeA, string nodeB)
+        {
+            int indexA = NodeLookup(nodeA);
+            int indexB = NodeLookup(nodeB);
+            bool neighbours = false;
+
+            if (indexA >= 0 && indexB >= 0)
+            {
+                if (Matrix[indexA][indexB] != 0)
+                {
+                    neighbours = true;
+                }
+            }
+
+            return neighbours;
+        }
+
+        public int NeighbourDistance(string nodeA, string nodeB)
+        {
+            int indexA = NodeLookup(nodeA);
+            int indexB = NodeLookup(nodeB);
+            int weight = int.MinValue;
+            bool neighbours = NeighbourCheck(nodeA, nodeB);
+
+            if (neighbours)
+            {
+                weight = Matrix[indexA][indexB];
+            }
+
+            return weight;
+        }
+
+        private int NodeLookup(string nodeName)
+        {
+            int index = int.MinValue;
+
+            for (int i = 0; i < Size; i++)
+            {
+                if (Nodes[i] == nodeName)
+                {
+                    index = i;
+                    i = int.MaxValue - 1;
+                }
+            }
+
+            return index;
+        }
+
+        private string EqualWrite(string input, int length)
+        {
+            return input.PadRight(length);
+        }
+    }
+}
